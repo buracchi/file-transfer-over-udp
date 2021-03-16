8823f77b5bdd2a708372a51674e632fc4e8b20eb
Fabio Buracchi <45599613+buracchi@users.noreply.github.com>
2021-03-16 08:32:23 +0100

    Refactory

--- server/fts.c
+++ server/fts.c
@@ -3,9 +3,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-#include <pthread.h>
-#include <unistd.h>
 #include <signal.h>
+#include <unistd.h>
 
 #include <event2/event.h>
 #include "socket2.h"
@@ -13,64 +12,81 @@
 #include "try.h"
 #include "utilities.h"
 
-static void listen_cb(evutil_socket_t fd, short what, void* arg);
-static void write_cb(evutil_socket_t fd, short what, void* arg);
-static void signal_cb(evutil_socket_t signal, short events, void* user_data);
+#define BACKLOG 4096
 
-static tpool_t tpool;
+struct dispatcher_thread_arg {
+	struct event_base* event_base;
+	socket2_t listening_socket;
+	tpool_t thread_pool;
+};
+
+struct request_handler_arg {
+	struct event_base* event_base;
+	socket2_t socket;
+};
+
+static void* handle_request(void* arg);
+static void dispatch_request(evutil_socket_t fd, short events, void* arg);
+static void fts_close(evutil_socket_t signal, short events, void* arg);
 
 extern int fts_start(int port, char* pathname) {
 	struct event_base* base;
 	struct event* socket_event;
 	struct event* signal_event;
+	struct dispatcher_thread_arg* dta;
+	tpool_t tpool;
 	socket2_t socket;
-	tpool = tpool_init(8);
+	try(tpool = tpool_init(8), NULL, fail);
 	try(socket = socket2_init(TCP, IPV4), NULL, fail);
 	try(socket2_ipv4_setaddr(socket, "127.0.0.1", port), 1, fail);
 	try(socket2_set_blocking(socket, false), 1, fail);
-	try(socket2_listen(socket, 256), 1, fail);
+	try(socket2_listen(socket, BACKLOG), 1, fail);
 	try(base = event_base_new(), NULL, fail);
-	try(socket_event = event_new(base, socket2_get_fd(socket), EV_READ | EV_PERSIST, listen_cb, socket), NULL, fail);
-	try(signal_event = evsignal_new(base, SIGINT, signal_cb, (void*)base), NULL, fail);
+	try(dta = malloc(sizeof * dta), NULL, fail);
+	dta->listening_socket = socket;
+	dta->thread_pool = tpool;
+	dta->event_base = base;
+	try(socket_event = event_new(base, socket2_get_fd(socket), EV_READ | EV_PERSIST, dispatch_request, dta), NULL, fail);
+	try(signal_event = evsignal_new(base, SIGINT, fts_close, (void*)base), NULL, fail);
 	try(event_add(socket_event, NULL), -1, fail);
 	try(event_add(signal_event, NULL), -1, fail);
-	printf("Server started.\n");
-	event_base_dispatch(base);
+	try((printf("Server started.\n") < 0), true, fail);
+	try(event_base_dispatch(base), -1, fail);
 	event_free(socket_event);
 	event_free(signal_event);
 	event_base_free(base);
 	libevent_global_shutdown();
-	socket2_close(socket);
+	free(dta);
+	try(socket2_close(socket), 1, fail);
 	socket2_destroy(socket);
-	tpool_destroy(tpool);
+	try(tpool_destroy(tpool), 1, fail);
 	return 0;
 fail:
 	return 1;
 }
 
-static void* foo(void* arg) {
-	socket2_t socket = arg;
+static void* handle_request(void* arg) {
+	struct request_handler_arg* rha = arg;
 	char* buff;
-	socket2_recv(socket, &buff);
+	socket2_recv(rha->socket, &buff);
 	sleep(1);
 	printf("%s\n", buff);
 	free(buff);
-	socket2_close(socket);
-	socket2_destroy(socket);
+	socket2_close(rha->socket);
+	socket2_destroy(rha->socket);
+	free(rha);
 }
 
-static void listen_cb(evutil_socket_t fd, short what, void* arg) {
-	socket2_t listener_socket = arg;
-	socket2_t accepted;
-	accepted = socket2_accept(listener_socket);
-	tpool_add_work(tpool, foo, accepted);
+static void dispatch_request(evutil_socket_t fd, short events, void* arg) {
+	struct dispatcher_thread_arg* dta = arg;
+	socket2_t accepted = socket2_accept(dta->listening_socket);
+	struct request_handler_arg* rha = malloc(sizeof * rha);
+	rha->event_base = dta->event_base;
+	rha->socket = accepted;
+	tpool_add_work(dta->thread_pool, handle_request, rha);
 }
 
-static void write_cb(evutil_socket_t fd, short what, void* arg) {
-
-}
-
-static void signal_cb(evutil_socket_t signal, short events, void* user_data) {
+static void fts_close(evutil_socket_t signal, short events, void* user_data) {
 	struct event_base* base = user_data;
 	struct timeval delay = { 2, 0 };
 	printf("\nShutting down the server...\n");
