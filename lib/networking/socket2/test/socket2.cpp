3d70478cc28ce52c78fa1fab3da92d15ef5f4d63
Fabio Buracchi <45599613+buracchi@users.noreply.github.com>
2021-08-21 17:49:35 +0200

    Update socket2.cpp

--- lib/networking/socket2/test/socket2.cpp
+++ lib/networking/socket2/test/socket2.cpp
@@ -13,33 +13,35 @@ extern "C" {
 #include <chrono>
 
 TEST(cmn_socket2, receive_message) {
+	std::mutex server_started;
 	std::mutex mtx;
 	std::string message = "MESSAGE";
+	server_started.lock();
 	mtx.lock();
 	std::string actual = "";
-	std::thread sender([&mtx, message] {
+	std::thread sender([&server_started, &mtx, message] {
 		cmn_socket2_t server;
 		cmn_socket2_t client;
 		try(server = cmn_socket2_init(cmn_nproto_service_ipv4, cmn_tproto_service_tcp), nullptr);
-		while (cmn_socket2_listen(server, "0.0.0.0:1234", 1024) && (errno == EADDRINUSE)) {
-			std::this_thread::sleep_for(std::chrono::milliseconds(50));
-		};
+		cmn_socket2_listen(server, "0.0.0.0:1234", 1024);
+		server_started.unlock();
 		try(client = cmn_socket2_accept(server), nullptr);
 		try(cmn_socket2_send(client, (const uint8_t*) message.c_str(), message.length()), -1);
 		mtx.lock();
-		try(cmn_socket2_destroy(server), !0);
-		try(cmn_socket2_destroy(client), !0);
+		cmn_socket2_destroy(server);
+		cmn_socket2_destroy(client);
 		return EXIT_SUCCESS;
 	});
-	std::thread receiver([&mtx, message, &actual] {
+	std::thread receiver([&server_started, &mtx, message, &actual] {
 		cmn_socket2_t socket;
 		char* buff = (char*) malloc(message.length() + 1);
 		memset(buff, 0, message.length() + 1);
 		try(socket = cmn_socket2_init(cmn_nproto_service_ipv4, cmn_tproto_service_tcp), nullptr);
-		try(cmn_socket2_connect(socket, "127.0.0.1:1234"), !0);
+		server_started.lock();
+		try(cmn_socket2_connect(socket, "127.0.0.1:1234"), -1);
 		try(cmn_socket2_recv(socket, (uint8_t*) buff, message.length()), -1);
 		mtx.unlock();
-		try(cmn_socket2_destroy(socket), !0);
+		cmn_socket2_destroy(socket);
 		actual = buff;
 		return EXIT_SUCCESS;
 	});
