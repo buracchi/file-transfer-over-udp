3bc9efddae9d004f501d084357637b161359f9f5
Fabio Buracchi <45599613+buracchi@users.noreply.github.com>
2021-08-27 03:50:13 +0200

    Update

--- ft-client/src/main.c
+++ ft-client/src/main.c
@@ -5,17 +5,17 @@
 #include "try.h"
 
 extern int main(int argc, const char* argv[]) {
-	char* url;
-	cmn_argparser_t argparser;
-	cmn_map_t option_map;
+    char* url;
+    cmn_argparser_t argparser;
+    cmn_map_t option_map;
     struct cmn_argparser_argument args[] = {
-        { .name = "url",	.help="the file transfer server URL address"},
+        { .name = "url",    .help="the file transfer server URL address" },
     };
     argparser = cmn_argparser_init(argv[0], "File transfer client.");
     cmn_argparser_set_arguments(argparser, args, 1);
     option_map = cmn_argparser_parse(argparser, argc, argv);
-	cmn_map_at(option_map, (void*)"url", (void**)&url);
+    cmn_map_at(option_map, (void*)"url", (void**)&url);
     cmn_argparser_destroy(argparser);
-	try(ftc_start(url), 1);
-	return 0;
+    try(ftc_start(url), 1);
+    return 0;
 }
--- ft-server/src/main.c
+++ ft-server/src/main.c
@@ -8,21 +8,21 @@
 #include "try.h"
 
 extern int main(int argc, const char* argv[]) {
-    cmn_argparser_t argparser;
+	cmn_argparser_t argparser;
 	cmn_map_t option_map;
-    struct cmn_argparser_argument args[] = {
-        { .flag = "u", .long_flag="url", 		.default_value="0.0.0.0:1234",	.help="specify the listening port number (the default value is \"0.0.0.0:1234\")"},
-        { .flag = "d", .long_flag="directory",	.default_value=getcwd(NULL, 0),	.help="specify the shared directory (the default value is the current working directory)"},
-    };
-    argparser = cmn_argparser_init(argv[0], "File transfer server.");
-    cmn_argparser_set_arguments(argparser, args, 2);
-    option_map = cmn_argparser_parse(argparser, argc, argv);
+	struct cmn_argparser_argument args[] = {
+		{ .flag = "u", .long_flag="url",		.default_value="0.0.0.0:1234",	.help="specify the listening port number (the default value is \"0.0.0.0:1234\")"},
+		{ .flag = "d", .long_flag="directory",	.default_value=getcwd(NULL, 0),	.help="specify the shared directory (the default value is the current working directory)"},
+	};
+	argparser = cmn_argparser_init(argv[0], "File transfer server.");
+	cmn_argparser_set_arguments(argparser, args, 2);
+	option_map = cmn_argparser_parse(argparser, argc, argv);
 	char* url;
 	char* directory;
 	cmn_map_at(option_map, (void*)"u", (void**)&url);
 	cmn_map_at(option_map, (void*)"d", (void**)&directory);
 	try(is_directory(directory), 0);
-    cmn_argparser_destroy(argparser);
+	cmn_argparser_destroy(argparser);
 	cmn_request_handler_t ft_handler;
 	cmn_communication_manager_t communication_manager;
 	try(ft_handler = (cmn_request_handler_t)ft_handler_init(directory), NULL);
